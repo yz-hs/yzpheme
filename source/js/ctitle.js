aaa676e6201258622b124096b072e58d86a067e7
yz-hs <yz_handsome0@163.com>
2020-08-21 23:51:24 +0800

    更新

--- source/js/ctitle.js
+++ source/js/ctitle.js
@@ -1,15 +1,13 @@
-var x = document.title;
-
-function sleep(d){
-  for(var t = Date.now();Date.now() - t <= d;);
-}
-
-window.onblur = function() {
-    document.title = "震惊！yz-hs竟...";
-};
-
-window.onfocus = function() {
-    document.title = "哈哈你回来了";
-    sleep(250);
-	document.title = x;
-};
+var OriginTitile = document.title;
+var titleTime;  
+document.addEventListener('visibilitychange', function(){  
+    if (document.hidden){  
+        document.title = '震惊！yz-hs竟...';  
+        clearTimeout(titleTime);  
+    }else{  
+        document.title = '哈哈你回来了';  
+        titleTime = setTimeout(function() {  
+            document.title = OriginTitile;  
+        }, 2000);
+    }  
+}); 
